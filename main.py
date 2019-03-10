a983b334da0e4e2919af33a056cc48c7bfdeae5b
fidansamet <fidansamet@hacettepe.edu.tr>
2019-03-10 11:15:59 +0300

    comment lines added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 hostName = "localhost"
 serverPort = 8080
+randomRange = 1000
 
 games = {}
 choices = ["Rock", "Paper", "Scissors"]
@@ -28,11 +29,13 @@ def RPS(me, you):
             return 0
 
 
-def findUniqueId(games):
-    sessionId = str(random.randint(0, 1000))
-    if sessionId in games:
-        findUniqueId(games)
-    else:
+def findUniqueId(games):        # no two games can have the same id, so we need to find a unique id
+    if randomRange == len(games):
+        return None
+    sessionId = str(random.randint(0, randomRange))
+    if sessionId in games:      # if random id is in use then try another random id
+        return findUniqueId(games)
+    else:                       # if random id is unique id then return it
         return sessionId
 
 
@@ -48,11 +51,17 @@ class MyServer(BaseHTTPRequestHandler):
 
         if command[0] == "/newGame":
             sessionId = findUniqueId(games)
+
+            # incase of there is no more unique id, give an error and quit the function
+            if sessionId == None:
+                self.wfile.write(bytes("\nYou exceed the number of total games. Please play unfinished games.\n\n", "utf-8"))
+                return
+
             roundNum = command[1].split("=")[1]
             rounds = []
             games[sessionId] = [int(roundNum), 0, rounds, 0, 0]   # totalRounds, currentRound, roundResults, wins, loses
 
-            if int(roundNum) <= 0:
+            if int(roundNum) <= 0:      # to play a game round number can not be equal or smaller than zero
                 self.wfile.write(bytes("\nRound number must be greater than 0 to start the game!\n\n", "utf-8"))
             else:
                 self.wfile.write(bytes("\nNew Rock-Paper-Scissors game started\n", "utf-8"))
@@ -66,6 +75,7 @@ class MyServer(BaseHTTPRequestHandler):
             playCommands = command[1].split("&")
             idOrChoose = playCommands[0].split("=")
 
+            # "id" and "choose" keywords have no order so check which one is first
             if idOrChoose[0] == "id":
                 id = idOrChoose[1]
                 choose = playCommands[1].split("=")[1]
@@ -74,10 +84,10 @@ class MyServer(BaseHTTPRequestHandler):
                 choose = idOrChoose[1]
                 id = playCommands[1].split("=")[1]
 
-            if id in games:
+            if id in games:         # if there is a game has that id, user can play it
                 gameInfo = games[id]
                 totalRound = gameInfo[0]
-                gameInfo[1] += 1
+                gameInfo[1] += 1        # increase round number
                 curRound = gameInfo[1]
                 me = choices[random.randint(0, 2)]
                 status = RPS(me.capitalize(), choose.capitalize())
@@ -86,19 +96,19 @@ class MyServer(BaseHTTPRequestHandler):
                 self.wfile.write(bytes("\nme: %s\n" % me.upper(), "utf-8"))
                 self.wfile.write(bytes("you: %s\n\n" % choose.upper(), "utf-8"))
 
-                if status == 0:
+                if status == 0:     # status lose
                     self.wfile.write(bytes("YOU LOST THIS ROUND!!\n", "utf-8"))
                     gameInfo[4] += 1
-                elif status == 1:   # status 1
+                elif status == 1:   # status win
                     self.wfile.write(bytes("YOU WON THIS ROUND!!\n", "utf-8"))
                     gameInfo[3] += 1
-                else:
+                else:       # status tie
                     self.wfile.write(bytes("TIE!!\n", "utf-8"))
 
                 gameInfo[2].append([me.capitalize(), choose.capitalize()])
                 rounds = gameInfo[2]
 
-                if curRound >= totalRound:
+                if curRound >= totalRound:      # user played all rounds
                     self.wfile.write(bytes("\n-> GAME COMPLETED", "utf-8"))
                     for i in range(totalRound):
                         self.wfile.write(bytes("\nRound %s: " % str(i+1), "utf-8"))
@@ -111,13 +121,13 @@ class MyServer(BaseHTTPRequestHandler):
                         self.wfile.write(bytes("YOU LOST !!\n\n", "utf-8"))
                     else:
                         self.wfile.write(bytes("TIE !!\n\n", "utf-8"))
-                    games.pop(id)
+                    games.pop(id)       # if game is completed then delete it
 
                 else:
                     self.wfile.write(bytes("\nThere is %s more round.\n\n" % str(totalRound-curRound), "utf-8"))
 
-            else:       # if there is no game has that id
-                self.wfile.write(bytes("\nYou need to create a game first.\n\n", "utf-8"))
+            else:       # if there is no game has that id give an error
+                self.wfile.write(bytes("\nFirst you need to create a new game.\n\n", "utf-8"))
 
 
 if __name__ == "__main__":
